e2aa50c8e78068c90c9ab1c4f3fe22bb2f187871
Deepshikhar <deepshikhar@Deepshikhars-MacBook-Pro.local>
2023-06-30 15:36:06 +0530

    base model prepared

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from cnnCDClassifier import logger
 from cnnCDClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnCDClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnCDClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+# from cnnCDClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
-STAGE_NAME = 'Data Ingestion Stage'
+
+STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
-
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
