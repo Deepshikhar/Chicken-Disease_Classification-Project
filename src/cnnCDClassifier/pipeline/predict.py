5787e569845f1def403d26dfd14b1e03f2c4fc57
deepshikhar <deepshikhar732000@gmail.con>
2023-07-02 23:06:38 +0530

    updated

--- src/cnnCDClassifier/pipeline/predict.py
+++ src/cnnCDClassifier/pipeline/predict.py
@@ -14,6 +14,7 @@ class PredictionPipeline:
     def predict(self):
         # load model
         model = load_model(os.path.join("artifacts","training", "model.h5"))
+        model = load_model("artifacts/training/model.h5")
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
